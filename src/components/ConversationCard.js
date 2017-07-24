36da254c59089ce9f9839cefe9d52a2590d428e0
victor-iyiola <javafolabi@gmail.com>
2017-07-24 01:40:53 +0100

    Refactored components to a functional component

--- src/components/ConversationCard.js
+++ src/components/ConversationCard.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TouchableNativeFeedback, Platform, View, Text } from 'react-native';
 import { styles } from '../styles';
 
-class ConversationCard extends Component {
-
-  render() {
-    return (
-      <TouchableNativeFeedback 
-          onPress={this.props.onPress} 
-          background={ Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : '' }>
-        <View style={styles.convCard}>
-          <Text style={styles.convCardText}>{this.props.text}</Text>
-        </View>
-      </TouchableNativeFeedback>
-    );
-  }
-
-}
+const ConversationCard = (props) => {
+  return (
+    <TouchableNativeFeedback
+        onPress={props.onPress}
+        background={ Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : '' }>
+      <View style={styles.convCard}>
+        <Text style={styles.convCardText}>{props.text}</Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
+};
 
 export default ConversationCard;
--- src/components/RoundButton.js
+++ src/components/RoundButton.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, Image, TouchableNativeFeedback, Platform } from 'react-native';
 import { styles } from '../styles';
 
-class RoundButton extends Component {
-  render() {
-    return (
-      <View>
-        <TouchableNativeFeedback
-            onPress={this.props.onPress}
-            background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : '' }>
-          <View style={styles.roundButton}>
-            <Image style={styles.roundButtonImg} source={this.props.img} />
-          </View>
-        </TouchableNativeFeedback>
-        <Text style={styles.roundButtonTitle}>{this.props.title}</Text>
-      </View>
-    );
-  }
-}
+const RoundButton = (props) => {
+  return (
+    <View>
+      <TouchableNativeFeedback
+          onPress={props.onPress}
+          background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : '' }>
+        <View style={styles.roundButton}>
+          <Image style={styles.roundButtonImg} source={props.img} />
+        </View>
+      </TouchableNativeFeedback>
+      <Text style={styles.roundButtonTitle}>{props.title}</Text>
+    </View>
+  );
+};
 
 export default RoundButton;
